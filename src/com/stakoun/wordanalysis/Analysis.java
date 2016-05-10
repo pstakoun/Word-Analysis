096109a9a418a16beb70a9ee4b02d518b16d7841
Peter Stakoun <peter@stakoun.com>
2016-05-09 21:32:34 -0400

    Initial commit

---
+++ src/com/stakoun/wordanalysis/Analysis.java
@@ -0,0 +1,15 @@
+package com.stakoun.wordanalysis;
+
+public class Analysis
+{
+	public Analysis(String[] text, String[] words)
+	{
+		// TODO
+	}
+	
+	public Analysis(String[] text, String[] words, String[] sectionSeperators)
+	{
+		// TODO
+	}
+
+}
---
+++ src/com/stakoun/wordanalysis/Main.java
@@ -0,0 +1,12 @@
+package com.stakoun.wordanalysis;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		// TODO get text and words from res files
+		Analysis richardiiiAnalysis = new Analysis(null, null);
+		Analysis fifthbusinessAnalysis = new Analysis(null, null);
+	}
+
+}
